f8d07b2d7a3f5f0660730f2dd3ded63f92267d76
Marsel Sampe Asang <marselsampe@gmail.com>
2019-06-02 00:37:40 +0700

    rewrite index.js to functional component

--- basic/src/index.js
+++ basic/src/index.js
@@ -5,31 +5,25 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import FunctionalLocalState from './FunctionalLocalState';
 import BindingLabelToInput from './BindingLabelToInput';
 
-class App extends React.Component {
-    constructor() {
-        super();
-    }
+function App() {
+    return (
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/functionlocalstate/">Function Local State</Link>
+                    </li>
+                    <li>
+                        <Link to="/bindinglabeltoinput/">Binding Label To Input</Link>
+                    </li>
+                </ul>
+                <hr />
 
-    render() {
-        return (
-            <Router>
-                <div>
-                    <ul>
-                        <li>
-                            <Link to="/functionlocalstate/">Function Local State</Link>
-                        </li>
-                        <li>
-                            <Link to="/bindinglabeltoinput/">Binding Label To Input</Link>
-                        </li>
-                    </ul>
-                    <hr/>
-
-                    <Route path="/functionlocalstate/" component={FunctionalLocalState} />
-                    <Route path="/bindinglabeltoinput/" component={BindingLabelToInput} />
-                </div>
-            </Router>
-        );
-    }
+                <Route path="/functionlocalstate/" component={FunctionalLocalState} />
+                <Route path="/bindinglabeltoinput/" component={BindingLabelToInput} />
+            </div>
+        </Router>
+    );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
