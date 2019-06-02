c471c18e2d1e37a097c61ade0f0fa0dd470a965e
Marsel Sampe Asang <marselsampe@gmail.com>
2019-06-02 12:51:06 +0700

    add missing onChange handler

--- basic/src/BindingLabelToInput.js
+++ basic/src/BindingLabelToInput.js
@@ -15,26 +15,33 @@ class MyInput extends React.Component {
     constructor(props) {
         super();
         this.id = getNextId();
+        this.onChange = this.onChange.bind(this);
     }
 
-    onChange(e){
-        this.props.onChange(e.target.value);
+    onChange(e) {
+        this.props.onChange(this.props.name, e.target.value);
     }
 
     render() {
         return (
             <label htmlFor={this.id}>
                 {this.props.label} :
-                <input id={this.id} value={this.props.value} onChange={this.onChange}/>
+                <input id={this.id} value={this.props.value} onChange={this.onChange} />
             </label>
         );
     }
 }
 
 class BindingLabelToInput extends React.Component {
-    constructor(){
+    constructor() {
         super();
         resetId();
+        this.state = { data1: 'data 1', data2: 'data 2', data3: 'data 3' };
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(stateName, newValue) {
+        this.setState({ [stateName]: newValue });
     }
 
     render() {
@@ -42,10 +49,10 @@ class BindingLabelToInput extends React.Component {
             <div>
                 <h3>Binding Label to Input</h3>
                 <code>Generate a sequential ID for each input/label pair.</code>
-                <hr/>
-                <MyInput label="Input 1" value="Data 1"/><br />
-                <MyInput label="Input 2" value="Data 2"/><br />
-                <MyInput label="Input 3" value="Data 3" />
+                <hr />
+                <MyInput label="Input 1" name="data1" value={this.state.data1} onChange={this.onInputChange} /><br />
+                <MyInput label="Input 2" name="data2" value={this.state.data2} onChange={this.onInputChange} /><br />
+                <MyInput label="Input 3" name="data3" value={this.state.data3} onChange={this.onInputChange} />
             </div>
         );
     }
